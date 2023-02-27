79ce725fee6da07a5c47565f2b2d3cf8c16f6e3c
Iftikhar Zahid <73811545+IftikharZahid@users.noreply.github.com>
2023-02-27 19:34:07 +0500

    login.jsx
    import { View, TouchableOpacity, Text, Button } from "react-native";
    
    function Login({ navigation }) {
    const onGoPressed = () => {
    navigation.navigate("profile");
    };
    
    return (
    <View style={{ backgroundColor: "gray", flex: 1 }}>
    <TouchableOpacity onPress={onGoPressed}>
    <Text style={{ fontSize: 22, color: "yellow", alignSelf: "center" }}>
    Go to Profile Page
    </Text>
    </TouchableOpacity>
    </View>
    );
    }
    
    export { Login };

---
+++ app.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Login } from "./src/screens/login";
+import { Profile } from "./src/screens/profile";
+
+export default function App() {
+const Stack = createNativeStackNavigator();
+return (
+<NavigationContainer>
+<Stack.Navigator>
+<Stack.Screen name="login" component={Login} />
+<Stack.Screen name="profile" component={Profile} />
+</Stack.Navigator>
+</NavigationContainer>
+);
+}
